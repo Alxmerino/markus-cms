1231dff79b01dc6d36ad136204b82be6930ce0ea
Rene Merino <rmerino@amayamedia.com>
2015-06-26 08:54:14 -0400

    Added sample controller

--- index.php
+++ index.php
@@ -26,6 +26,12 @@ require 'vendor/autoload.php';
 $router = new Phroute\RouteCollector();
 
 /**
+ * The file system
+ */
+use League\Flysystem\Filesystem;
+use League\Flysystem\Adapter\Local as Adapter;
+
+/**
  * Filters
  */
 $router->filter('auth', function() {
@@ -37,13 +43,57 @@ $router->filter('auth', function() {
 });
 
 /**
+ * Files controller
+ */
+class FileController {
+
+	protected $fs;
+
+	public function __construct() {
+		$this->fs = new Filesystem(new Adapter(__DIR__));
+	}
+
+    public function anyIndex()
+    {
+    	$this->fs->write('index.txt', 'contents');
+        // return 'This is the default page and will respond to /controller and /controller/index';
+    }
+
+    /**
+    * One required paramter and one optional parameter
+    */
+    public function anyFileController($param, $param2 = 'default')
+    {
+        return 'This will respond to /controller/test/{param}/{param2}? with any method';
+    }
+
+    public function getFileController()
+    {
+        return 'This will respond to /controller/test with only a GET method';
+    }
+
+    public function postFileController()
+    {
+        return 'This will respond to /controller/test with only a POST method';
+    }
+
+    public function putFileController()
+    {
+        return 'This will respond to /controller/test with only a PUT method';
+    }
+
+    public function deleteFileController()
+    {
+        return 'This will respond to /controller/test with only a DELETE method';
+    }
+}
+
+/**
  * Define routes
  */
 $router->group(array('before' => 'auth'), function($router) {
-	$router->get('/', function() {
-		// Add this to controller
-		return 'Main admin page';
-	});
+
+	$router->controller('/', 'FileController');
 });
 
 // Login route
@@ -52,12 +102,10 @@ $router->get('/login', function() {
 	return 'Auth the user here';
 });
 
-
 /**
  * Dispatch router
  */
 $dispatcher = new Phroute\Dispatcher($router);
-
 
 /**
  * Print our response from dispatcher
