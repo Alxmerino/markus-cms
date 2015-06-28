b487e91dc93c6771113b715946839b75cc15d76b
Rene Merino <rmerino@amayamedia.com>
2015-06-27 21:18:13 -0400

    Restructured app, use less classes and list files

--- app/Markus/Routes/Routes.php
+++ app/Markus/Routes/Routes.php
@@ -1,9 +1,10 @@
 <?php 
 
 namespace Markus\Routes;
-use Markus\Filters\AuthFilter;
 use Phroute\RouteCollector as Phrouter;
 use Phroute\Dispatcher as Dispatcher;
+use League\Flysystem\Filesystem;
+use League\Flysystem\Adapter\Local as Adapter;
 
 /**
 * Handle Routes
@@ -15,12 +16,15 @@ class Routes
 	// @var Dispatcher
 	public $dispatcher;
 	// @var Filters
-	public $filters;
+	public $filesystem;
+	// @array CMS config
+	private $config = array();
 	
 	function __construct()
 	{
+		$this->filesystem = new Filesystem(new Adapter($_SERVER['DOCUMENT_ROOT']));
 		$this->router = $this->routes(new Phrouter());
-		$this->filters = new AuthFilter($this->router);
+		$this->filters($this->router);
 		$this->dispatcher = new Dispatcher($this->router);
 		$this->serveResponse($this->dispatcher);
 	}
@@ -32,21 +36,46 @@ class Routes
 	 */
 	function routes($router)
 	{
-
+		/**
+		 * Login route
+		 */
 		$router->get('/login', function() {
 			return "You must login to proceed";
 		});
 
+		// Main route
 		$router->group(array('before' => 'auth'), function($router) {
 
 			$router->get('/', function() {
-				return 'Hello Admin';
+				
+				/* Get config data */
+				$this->config = $this->getConfig();
+				$contents = $this->filesystem->listContents($this->config->app_path);
+				echo '<pre>';
+	print_r($contents);
+echo '</pre>';
 			});
 			// $router->controller('/', 'FileController');
 		});
 
 		// Return the router
 		return $router;
+	}
+
+	/**
+	 * Get config data
+	 * @return stdClass
+	 */
+	function getConfig()
+	{
+		return json_decode($this->filesystem->read('.config'));
+	}
+
+	function filters($router)
+	{
+		$router->filter('auth', function() {
+			// Set login session here
+		});
 	}
 
 	function serveResponse($dispatcher)
